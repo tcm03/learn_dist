6075d151187b8e412d96f02215ada695e35455f9
tcm03 <21125019@student.hcmus.edu.vn>
2025-02-05 16:11:30 +0700

    FC(1000, 70000) for batch 10 on 28GB

--- nnetwork.py
+++ nnetwork.py
@@ -4,8 +4,8 @@ class MLP(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc_layers = nn.Sequential(
-            nn.Linear(1000, 35000),
-            nn.Linear(35000, 1000)
+            nn.Linear(1000, 70000),
+            nn.Linear(70000, 1000)
         )
         
     def forward(self, x):
